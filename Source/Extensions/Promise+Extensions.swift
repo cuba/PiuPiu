30a8bdad4c6a1a2182bb05b7e6ac5ed5b0aff791
Jacob Sikorski <jacob.sikorski@gmail.com>
2019-02-17 01:08:32 -0500

    Add error deserialization

--- Source/Extensions/Promise+Extensions.swift
+++ Source/Extensions/Promise+Extensions.swift
@@ -109,6 +109,85 @@ extension Promise where T == SuccessResponse<Data?> {
     }
 }
 
+extension Promise where E == ErrorResponse<Data?> {
+    
+    /// Attempts to unwrap the resposne data.
+    ///
+    /// - Returns: A promose containing the unwrapped data
+    open func unwrapErrorData() -> Promise<T, ErrorResponse<Data>> {
+        return thenFailure() { response in
+            // Check if we have the data we need
+            guard let unwrappedData = response.data else {
+                throw SerializationError.unexpectedEmptyResponse
+            }
+            
+            return (unwrappedData, response.httpResponse, response.urlRequest, response.statusCode, response.error)
+        }
+    }
+    
+    /// Attempts to deserialize the resposne data into a JSON string.
+    ///
+    /// - Returns: A promose containing the deserilized results
+    open func deserializeJSONString() -> Promise<T, ErrorResponse<String>> {
+        return unwrapErrorData().thenFailure { response in
+            // Attempt to deserialize the object.
+            guard let jsonString = String(data: response.data, encoding: .utf8) else {
+                throw SerializationError.failedToDecodeResponseData(cause: nil)
+            }
+            
+            return (jsonString, response.httpResponse, response.urlRequest, response.statusCode, response.error)
+        }
+    }
+    
+    /// Attempts to deserialize the resposne data into a MapDecodable object.
+    ///
+    /// - Returns: A promose containing the deserilized results
+    open func deserializeError<D: MapDecodable>(to: D.Type) -> Promise<T, ErrorResponse<D>> {
+        return unwrapErrorData().thenFailure { response in
+            do {
+                // Attempt to deserialize the object.
+                let object = try D(jsonData: response.data)
+                return (object, response.httpResponse, response.urlRequest, response.statusCode, response.error)
+            } catch {
+                // Wrap this error so that we're controlling the error type and return a safe message to the user.
+                throw SerializationError.failedToDecodeResponseData(cause: error)
+            }
+        }
+    }
+    
+    /// Attempts to deserialize the resposne data into a MapDecodable array.
+    ///
+    /// - Returns: A promose containing the deserilized results
+    open func deserializeError<D: MapDecodable>(to: [D].Type) -> Promise<T, ErrorResponse<[D]>> {
+        return unwrapErrorData().thenFailure { response in
+            do {
+                // Attempt to deserialize the object.
+                let object = try D.parseArray(jsonData: response.data)
+                return (object, response.httpResponse, response.urlRequest, response.statusCode, response.error)
+            } catch {
+                // Wrap this error so that we're controlling the error type and return a safe message to the user.
+                throw SerializationError.failedToDecodeResponseData(cause: error)
+            }
+        }
+    }
+    
+    /// Attempts to deserialize the resposne data into a Decodable object.
+    ///
+    /// - Returns: A promose containing the deserilized results
+    open func deserializeError<D: Decodable>(to: D.Type) -> Promise<T, ErrorResponse<D>> {
+        return unwrapErrorData().thenFailure { response in
+            do {
+                // Attempt to deserialize the object.
+                let object = try JSONDecoder().decode(D.self, from: response.data)
+                return (object, response.httpResponse, response.urlRequest, response.statusCode, response.error)
+            } catch {
+                // Wrap this error so that we're controlling the error type and return a safe message to the user.
+                throw SerializationError.failedToDecodeResponseData(cause: error)
+            }
+        }
+    }
+}
+
 extension Promise {
     
     /// Calls the start() method on the promise
