414e3019bff2fdbf5312078ea10f61ccb7ab3d04
Jacob Sikorski <jacob.sikorski@gmail.com>
2019-03-02 10:01:04 -0500

    Add more status codes

--- Source/Enums/StatusCode.swift
+++ Source/Enums/StatusCode.swift
@@ -12,57 +12,113 @@ import Foundation
 ///
 /// - ok: 200
 /// - created: 201
-/// - noData: 204
+/// - accepted: 202
+/// - noContent: 204
+/// - resetContent: 205
+/// - partialContent: 206
+/// - multiStatus: 207
+/// - alreadyReported: 208
+/// - imUsed: 226
 /// - badRequest: 400
 /// - unauthorized: 401
+/// - paymentRequired: 402
 /// - forbidden: 403
 /// - notFound: 404
+/// - methodNotAllowed: 405
+/// - notAcceptable: 406
 /// - unprocessableEntity: 422
 /// - conflict: 409
+/// - gone: 410
+/// - lengthRequired: 411
+/// - unsupportedMediaType: 415
 /// - internalServerError: 500
+/// - notImplemented: 501
+/// - badGateway: 502
+/// - serviceUnavailable: 503
+/// - gatewayTimeout: 504
+/// - httpVersionNotSupported: 505
 /// - other: Any status codes not covered by this enum.
 public enum StatusCode: Equatable {
+    
+    static let predefined: [StatusCode] = [
+        .ok, .created, .accepted, .noContent, .resetContent, .partialContent, .multiStatus, .alreadyReported, .imUsed,
+        .badRequest, .unauthorized, .paymentRequired, .forbidden, .notFound, .methodNotAllowed, .notAcceptable, .unprocessableEntity, .conflict, .gone, .lengthRequired, .unsupportedMediaType,
+        .internalServerError, .notImplemented, .badGateway, .serviceUnavailable, .gatewayTimeout, .httpVersionNotSupported
+    ]
+    
+    /// 2xx
     case ok
     case created
-    case noData
+    case accepted
+    case noContent
+    case resetContent
+    case partialContent
+    case multiStatus
+    case alreadyReported
+    case imUsed
+    
+    /// 4xx
     case badRequest
     case unauthorized
+    case paymentRequired
     case forbidden
     case notFound
+    case methodNotAllowed
+    case notAcceptable
     case unprocessableEntity
     case conflict
+    case gone
+    case lengthRequired
+    case unsupportedMediaType
+    
+    /// 5xx
     case internalServerError
+    case notImplemented
+    case badGateway
+    case serviceUnavailable
+    case gatewayTimeout
+    case httpVersionNotSupported
+    
     case other(Int)
     
-    var rawValue: Int {
+    public var rawValue: Int {
         switch self {
-        case .ok                    : return 200
-        case .created               : return 201
-        case .noData                : return 204
-        case .badRequest            : return 400
-        case .unauthorized          : return 401
-        case .forbidden             : return 403
-        case .notFound              : return 404
-        case .unprocessableEntity   : return 422
-        case .conflict              : return 409
-        case .internalServerError   : return 500
-        case .other(let value)      : return value
+        case .ok                        : return 200
+        case .created                   : return 201
+        case .accepted                  : return 202
+        case .noContent                 : return 204
+        case .resetContent              : return 205
+        case .partialContent            : return 206
+        case .multiStatus               : return 207
+        case .alreadyReported           : return 208
+        case .imUsed                    : return 226
+        case .badRequest                : return 400
+        case .unauthorized              : return 401
+        case .paymentRequired           : return 402
+        case .forbidden                 : return 403
+        case .notFound                  : return 404
+        case .methodNotAllowed          : return 405
+        case .notAcceptable             : return 406
+        case .unprocessableEntity       : return 422
+        case .conflict                  : return 409
+        case .gone                      : return 410
+        case .lengthRequired            : return 411
+        case .unsupportedMediaType      : return 415
+        case .internalServerError       : return 500
+        case .notImplemented            : return 501
+        case .badGateway                : return 502
+        case .serviceUnavailable        : return 503
+        case .gatewayTimeout            : return 504
+        case .httpVersionNotSupported   : return 505
+        case .other(let value)          : return value
         }
     }
     
-    init(rawValue: Int) {
-        switch rawValue {
-        case StatusCode.ok.rawValue                     : self = .ok
-        case StatusCode.created.rawValue                : self = .created
-        case StatusCode.noData.rawValue                 : self = .noData
-        case StatusCode.badRequest.rawValue             : self = .badRequest
-        case StatusCode.unauthorized.rawValue           : self = .unauthorized
-        case StatusCode.forbidden.rawValue              : self = .forbidden
-        case StatusCode.notFound.rawValue               : self = .notFound
-        case StatusCode.conflict.rawValue               : self = .conflict
-        case StatusCode.unprocessableEntity.rawValue    : self = .unprocessableEntity
-        case StatusCode.internalServerError.rawValue    : self = .internalServerError
-        default                                         : self = .other(rawValue)
+    public init(rawValue: Int) {
+        if let statusCode = StatusCode.predefined.first(where: { $0.rawValue == rawValue }) {
+            self = statusCode
+        } else {
+            self = StatusCode.other(rawValue)
         }
     }
     
