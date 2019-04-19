c5f47561df499e7201e4e3d686e6554a3eb4097b
Jacob Sikorski <jacob.sikorski@workjam.com>
2019-04-19 18:53:41 -0400

    Add support for other devices

---
+++ PewPew tvOS/PewPew_tvOS.h
@@ -0,0 +1,19 @@
+//
+//  PewPew_tvOS.h
+//  PewPew tvOS
+//
+//  Created by Jacob Sikorski on 2019-04-19.
+//  Copyright © 2019 Jacob Sikorski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PewPew_tvOS.
+FOUNDATION_EXPORT double PewPew_tvOSVersionNumber;
+
+//! Project version string for PewPew_tvOS.
+FOUNDATION_EXPORT const unsigned char PewPew_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PewPew_tvOS/PublicHeader.h>
+
+
