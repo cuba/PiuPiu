0576475854f9c25538afd115ec76f8263d7ea6ec
Jacob Sikorski <jacob.sikorski@gmail.com>
2020-07-26 00:12:26 -0600

    Fix computing of percentages

--- Source/Extensions/URLSessionTask+Extensions.swift
+++ Source/Extensions/URLSessionTask+Extensions.swift
@@ -9,93 +9,90 @@
 import Foundation
 
 public extension URLSessionTask {
-    var safeCountOfBytesClientExpectsToReceive: Int64 {
-        #if os(iOS)
-        if #available(iOSApplicationExtension 11.0, *) {
-            return countOfBytesClientExpectsToReceive
-        } else {
-            return countOfBytesExpectedToReceive
-        }
-        #elseif os(watchOS)
-        if #available(watchOSApplicationExtension 4.0, *) {
-            return countOfBytesClientExpectsToReceive
-        } else {
-            return countOfBytesExpectedToReceive
-        }
-        #elseif os(tvOS)
-        if #available(tvOSApplicationExtension 11.0, *) {
-            return countOfBytesClientExpectsToReceive
-        } else {
-            return countOfBytesExpectedToReceive
-        }
-        #elseif os(macOS)
-        if #available(OSXApplicationExtension 10.13, *) {
-            return countOfBytesClientExpectsToReceive
-        } else {
-            return countOfBytesExpectedToReceive
-        }
-        #endif
-    }
-    
-    var safeCountOfBytesClientExpectsToSend: Int64 {
-        #if os(iOS)
-        if #available(iOSApplicationExtension 11.0, *) {
-            return countOfBytesClientExpectsToSend
-        } else {
-            return countOfBytesExpectedToSend
-        }
-        #elseif os(watchOS)
-        if #available(watchOSApplicationExtension 4.0, *) {
-            return countOfBytesClientExpectsToSend
-        } else {
-            return countOfBytesExpectedToSend
-        }
-        #elseif os(tvOS)
-        if #available(tvOSApplicationExtension 11.0, *) {
-            return countOfBytesClientExpectsToSend
-        } else {
-            return countOfBytesExpectedToSend
-        }
-        #elseif os(macOS)
-        if #available(OSXApplicationExtension 10.13, *) {
-            return countOfBytesClientExpectsToSend
-        } else {
-            return countOfBytesExpectedToSend
-        }
-        #endif
-    }
-    
+    /// Calculates the percent sent based on `countOfBytesExpectedToSend` Returns nil if `countOfBytesExpectedToSend <= 0`
     var percentSent: Float? {
-        let expectedToSend = safeCountOfBytesClientExpectsToSend
-        
-        if expectedToSend > 0 {
-            return Float(Double(integerLiteral: countOfBytesSent) / Double(integerLiteral: expectedToSend))
+        if countOfBytesExpectedToSend > 0 {
+            return Float(Double(integerLiteral: countOfBytesSent) / Double(integerLiteral: countOfBytesExpectedToSend))
         } else {
             return nil
         }
     }
     
+    /// Calculates the percent sent based on `countOfBytesExpectedToReceive` Returns nil if `countOfBytesExpectedToReceive <= 0`
     var percentRecieved: Float? {
-        let expectedToReceive = safeCountOfBytesClientExpectsToReceive
-        
-        if expectedToReceive > 0 {
-            return Float(Double(integerLiteral: countOfBytesReceived) / Double(integerLiteral: expectedToReceive))
+        if countOfBytesExpectedToReceive > 0 {
+            return Float(Double(integerLiteral: countOfBytesReceived) / Double(integerLiteral: countOfBytesExpectedToReceive))
         } else {
             return nil
         }
     }
     
+    /// Calculates the percent sent based on `safeCountOfBytesClientExpectsToSend` and `safeCountOfBytesClientExpectsToReceive` Returns 0 if `safeCountOfBytesClientExpectsToSend <= 0` and `safeCountOfBytesClientExpectsToReceive` <= 0
     var percentTransferred: Float? {
-        let expectedToSend = safeCountOfBytesClientExpectsToSend
-        let expectedToReceive = safeCountOfBytesClientExpectsToReceive
-        let expectedToTransfer = expectedToSend + expectedToReceive
+        if let percentSent = self.percentSent, let percentRecieved = self.percentRecieved {
+            return (percentSent + percentRecieved) / 2
+        } else if let percentSent = self.percentSent {
+            return percentSent
+        } else {
+            return percentRecieved
+        }
+    }
+}
+
+public extension Sequence where Iterator.Element: URLSessionTask {
+    /// Calculates the average percent sent for all elements that return a value for `percentSent`
+    var averagePercentSent: Float? {
+        let percentages = compactMap({ $0.percentSent })
         
-        if expectedToTransfer > 0 {
-            let dataTransferred = countOfBytesReceived + countOfBytesSent
-            let progress = Float(Double(integerLiteral: dataTransferred) / Double(integerLiteral: expectedToTransfer))
-            return progress
+        if !percentages.isEmpty {
+            let total = percentages.reduce(0, { $0 + $1 })
+            return total / Float(percentages.count)
         } else {
             return nil
         }
+    }
+    
+    /// Calculates the average percent sent for all elements that return a value for `percentRecieved`
+    var averagePercentRecieved: Float? {
+        let percentages = compactMap({ $0.percentRecieved })
+        
+        if !percentages.isEmpty {
+            let total = percentages.reduce(0, { $0 + $1 })
+            return total / Float(percentages.count)
+        } else {
+            return nil
+        }
+    }
+    
+    /// Calculates the average percent sent for all elements that return a value for `percentTransferred`
+    var averagePercentTransferred: Float? {
+        let percentages = compactMap({ $0.percentTransferred })
+        
+        if !percentages.isEmpty {
+            let total = percentages.reduce(0, { $0 + $1 })
+            return total / Float(percentages.count)
+        } else {
+            return nil
+        }
+    }
+    
+    /// Returns all tasks that have `URLSessionTask.State` of `.completed`
+    var completed: [URLSessionTask] {
+        return self.filter({ $0.state == .completed })
+    }
+    
+    /// Returns all tasks that have `URLSessionTask.State` of `.canceling`
+    var cancelling: [URLSessionTask] {
+        return self.filter({ $0.state == .canceling })
+    }
+    
+    /// Returns all tasks that have `URLSessionTask.State` of `.running`
+    var running: [URLSessionTask] {
+        return self.filter({ $0.state == .running })
+    }
+    
+    /// Returns all tasks that have `URLSessionTask.State` of `.suspended`
+    var suspended: [URLSessionTask] {
+        return self.filter({ $0.state == .suspended })
     }
 }
